c62fb0a27167845a225514aed33b9a13152cf42f
Arul Johnson <aruljohnson9411@gmail.com>
2022-12-05 05:28:06 +0530

    initial
    This web page is not responsive .Since there was time limit was unable to implement jquery . But can inplement if given one more day .

---
+++ script.js
@@ -0,0 +1,31 @@
+var animation = bodymovin.loadAnimation({
+
+    container: document.getElementById('animation-container'),
+    
+    path: 'Freelancer.json',
+    
+    renderer: 'svg',
+    
+    loop: true,
+    
+    autoplay: true,
+    
+    name: "Demo Animation",
+    
+    });     
+
+    var anim = bodymovin.loadAnimation({
+
+        container: document.getElementById('animation-container'),
+        
+        path: 'Freelancer.json',
+        
+        renderer: 'svg',
+        
+        loop: true,
+        
+        autoplay: true,
+        
+        name: "Demo Animation",
+        
+        });  
